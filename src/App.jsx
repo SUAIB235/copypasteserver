79e60183119ed007d20f7e469dd1afcfd60a8931
SUAIB235 <suaibhasan056@gmail.com>
2026-05-04 03:34:58 +0600

    Open link in new tab on button click
    Updated button click handler to open link in a new tab.

--- src/App.jsx
+++ src/App.jsx
@@ -215,7 +215,7 @@ export default function App() {
 
             <button
               onClick={() =>
-                window.open("https://drive.google.com/file/d/1tT9eLOoWFW9UemD65WAgtCKaBddbH742/view?usp=drivesdk")
+                window.open("https://drive.google.com/file/d/1tT9eLOoWFW9UemD65WAgtCKaBddbH742/view?usp=drivesdk" , "_blank")
               }
               className="bg-gray-100 px-4 py-3 rounded-lg w-full text-left"
             >
